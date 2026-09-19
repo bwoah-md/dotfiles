3b371ca23ac5501cd1ae643e88ea33d95e48be3b
bwoah-md <143240188+bwoah-md@users.noreply.github.com>
2026-09-19 09:33:22 +0530

    labwc: updated menus and icons

--- labwc/template/apply.sh
+++ labwc/template/apply.sh
@@ -74,6 +74,7 @@ EOF
     create_primary_icon "reload-2.svg" "reload-2.svg"
     create_primary_icon "wallpaper.svg" "wallpaper.svg"
     create_primary_icon "settings.svg" "settings.svg"
+    create_primary_icon "sync.svg" "sync.svg"
     create_primary_icon "logout.svg" "logout.svg"
 
     # Client menu icons (Primary)
